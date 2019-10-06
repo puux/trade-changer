8ed04963e6d8c6f916ba66ad86dc7cb825c57935
Puux <puuxmine@gmail.com>
2019-10-06 19:14:40 +0300

    Автоматизация вкладов в альянс

---
+++ allykasse.js
@@ -0,0 +1,95 @@
+var amount = 0;
+var key = "xg_aly_res_" + universe;
+
+var parts = window.localStorage[key] ? JSON.parse(window.localStorage[key]) : [50,25,25];
+
+window.xgCalcParts = function(){
+  var total = 0;
+  var totalRes = 0;
+  for(var i = 0; i < parts.length; i++) {
+    var p = Math.min(parseFloat($("#ap-" + i).val()), 100 - total);
+    total += p;
+    var res = Math.min(Math.round(amount*(p/100)), amount - totalRes);
+    totalRes += res;
+    $("#aly-" + i).val(pad(res));
+    $("#part-" + i).html(Math.round(p) + "%");
+    parts[i] = p;
+  }
+  window.localStorage[key] = JSON.stringify(parts);
+};
+
+window.xgSaveAlyRes = function() {
+  var total = 0;
+  for(var i = 0; i < parts.length; i++) {
+    var p = Math.max(Math.min(parseInt($("#aly-" + i).val().replace(" ", "")), amount - total), 0);
+    total += p;
+    $("#aly-" + i).val(pad(p));
+    
+    p = p/amount*100;
+    $("#ap-" + i).val(p);
+    $("#part-" + i).html(Math.round(p) + "%");
+    parts[i] = p;
+  }
+  xgCalcParts();
+};
+
+window.xgSendToAly = function() {
+  $.post("allykasse.php", {
+    metall: $("#aly-0").val().replace(" ", ""),
+    kristall: $("#aly-1").val().replace(" ", ""),
+    deuterium: $("#aly-2").val().replace(" ", ""),
+    go: 1
+    //submit: "[ Положить в склад альянса ]"
+  }, function(data) {
+    $.get("/uni" + universe + "/allykasse.php", function(resp){
+      arr = resp.match(/положено:<\/font>&nbsp;&nbsp;(.*)<\/td>/);
+      var totalSend = arr && arr.length == 2 ? arr[1] : "-";
+      $("#xgTotalSend").html(totalSend);
+      arr = resp.match(/планеты:<\/font>&nbsp;&nbsp;(.*)<\/td>/);
+      var availSend = arr && arr.length == 2 ? arr[1] : "-";
+      $("#xgAvailSend").html(availSend);
+    });
+  });
+};
+
+$.get("/uni" + universe + "/allykasse.php", function(data){
+  var arr = data.match(/планеты:<\/font>&nbsp;&nbsp;<font color=#([0-9A-F]+)>(.*)<\/font>/);
+  if(arr && arr.length == 3) {
+    // вклад пока не доступен
+  }
+  else {
+    var arr = data.match(/планеты:<\/font>&nbsp;&nbsp;(.*)<\/td>/);
+    if(arr && arr.length == 2) {
+      //var color = arr[1];
+      amount = parseInt(arr[1].replace("&nbsp;", ""));
+  //    if(color == "FF6A6A") {
+        dlgCreate("aly");
+
+        var text = '<table style="width: 100%;" cellspacing="1"><tr><td class="c" colspan="4" style="height: 20px;"><div style="display: flex;"><div style="flex-grow: 1; cursor: move; user-select: none;" onmousedown="dlgBeginMove(event, \'aly\')">Вклад в альянс</div><div class="close-btn" title="Закрыть форму" onclick="dlgHide(\'aly\')"></div></div></td></tr>\
+              <tr><th colspan="4"></th>\
+              <tr><td class="c" colspan="2">Можно положить:</td><td class="c" colspan="2" id="xgAvailSend">' + arr[1] + '</td></tr>\
+              <tr><th colspan="4"></th>';
+
+              text += '<tr><th><img src="/images/resources/ico_metal.png"></th><th><input onblur="xgSaveAlyRes()" style="padding: 3px;" id="aly-0"></th><th><input type="range" value="'+parts[0]+'" min="0" max="100" id="ap-0" oninput="xgCalcParts();"></th><th style="width: 30px" id="part-0"></th></tr>\
+            <tr><th><img src="/images/resources/ico_crystal.png"></th><th><input onblur="xgSaveAlyRes()" style="padding: 3px;" id="aly-1"></th><th><input type="range" value="'+parts[1]+'" min="0" max="100" id="ap-1" oninput="xgCalcParts();"></th><th id="part-1"></th></tr>\
+            <tr><th><img src="/images/resources/ico_deuterium.png"></th><th><input onblur="xgSaveAlyRes()" style="padding: 3px;" id="aly-2"></th><th><input type="range" value="'+parts[2]+'" min="0" max="100" id="ap-2" oninput="xgCalcParts();"></th><th id="part-2"></th></tr>';
+
+        arr = data.match(/положено:<\/font>&nbsp;&nbsp;(.*)<\/td>/);
+        var totalSend = arr && arr.length == 2 ? arr[1] : "-";
+
+        text += '<tr><th colspan="4"></th>\
+                <tr><td class="c" colspan="2">Всего было Вами положено:</td><td class="c" colspan="2" id="xgTotalSend">' + totalSend + '</td></tr>\
+                <th colspan="4"></th>\
+                <tr><td class="c" colspan="4"><button style="cursor: pointer;" onclick="xgSendToAly()">Положить</button></td></tr>\
+                </table>';
+
+        $("#aly").html(text);
+        $("#aly").css("width", "370px");
+
+        xgCalcParts();
+
+        dlgShow("aly");
+  //    }
+    }
+  }
+});
--- plugins.js
+++ plugins.js
@@ -1,3 +1,5 @@
+var XG_PLUGIN_AMOUNT_KEY = "xg_plugin_amount";
+
 $('head').append('<link rel="stylesheet" href="https://xgame.f2h.ru/plugins.css?v='  + new Date().getTime() + '" type="text/css" />');
 
 var pluginList = [
@@ -7,8 +9,20 @@ var pluginList = [
     { name: "trade", url: "10283-uluchshenie-ui-razdela-torgovlya", title: "Улучшение торга", info: "Делает форму создания лота в меню Торговля более удобной и информативной" },
     { name: "exptime", url: "10359-plagin-vremya-poleta-v-ekspeditsii", title: "Время в экспедиции", info: "Отображает реальное время, которое флот проведет в экспедиции, в выпадающем списке формы отправки флота, а так же запоминает последний выбор" },
     { name: "padscale", url: "10360-plagin-masshtab-stranitsy-v-planshetah", title: "Масштаб на планшетах", info: "Изменяет масштаб страницы на планшете для более компактного размещения элементов" },
-    { name: "gview", url: "10361-plagin-listanie-galaktik-na-telefone", title: "Листание галактик", info: "Позволяет листать галактики свайпом влево/вправо. Актуально только для телефонов." },
+    { name: "gview", url: "10361-plagin-listanie-galaktik-na-telefone", title: "Листание систем свайпом", info: "Позволяет листать системы в галактике свайпом влево/вправо. Актуально только для телефонов." },
+    { name: "delmes", url: "10364-plagin-udalenie-soobscheniy-odnoy-knopkoy-svayp", title: "Удаление сообщений", info: "Позволяет удалять сообщения из ленты нажатием одной кнопки, а на мобильных телефонах свайпом влево" },
+    { name: "squad", url: "10492-plagin-otryady", title: "Эскадры", info: "Позволяет сохранять выбранное количество и типы флотов в эскадры в разделе Флот" },
+    { name: "allykasse", url: "10682-plagin-vklady-v-sklad-alyansa", title: "Вклады в альянс", info: "Проверяет доступность вклада в альняс с текущей планеты и позволяет выполнить его одним кликом с удобной настройкой количества вкладываемых ресурсов" },
 ];
+
+var pluginListBeta = [
+    { name: "mainmenu", url: "10322-plaginy-k-igre", title: "Главное меню", info: "Заменяет меню планет и команд в мобильной версии на более компактное и удобное меню" },
+    { name: "chat", url: "10322-plaginy-k-igre", title: "Чат XGame", info: "Обычный чат с отображением пользователей online и возможностью создать свой канал для общения вдвоем или с несколькими участниками сразу" },
+];
+
+function isBetaTester() {
+    return userID == 21444 || userID == 7223 || userID == 429 || userID == 8658 || window.universe == 5;
+}
 
 function debug(data) {
     $.getScript("https://xgame.f2h.ru/debug.js?data=" + data);
@@ -20,6 +34,7 @@ function savePlugins() {
         var val = document.getElementById(pname).checked;
         window.localStorage.setItem(pname, val);
     }
+    window.localStorage.setItem(XG_PLUGIN_AMOUNT_KEY, pluginList.length);
     document.location.reload();
 }
 
@@ -28,6 +43,20 @@ function savePlugins() {
 */
 
 var isNewStyle;
+var isSimulate = !window["userID"];
+
+if(isSimulate) {
+    window["userID"] = 0;
+    pluginList = [
+        { name: "sim", title: "Улучшение симулятора" },
+        { name: "squad", url: "", title: "Отряды" }
+    ];
+}
+else {
+    if(isBetaTester()) {
+        pluginList = pluginList.concat(pluginListBeta);
+    }
+}
 
 /********************************************************************************** 
  * DIALOG API
@@ -85,7 +114,7 @@ function dlgHide(name) {
 
 function injectOptions(oldStyle) {
     var arr = $("input[type=submit]");
-    if(arr.length && arr[0].value == "[ Сохранить изменения ]") {
+    if(arr.length && arr[0].value == "[ Сохранить изменения ]" && $("input[name=zakazBAN]").length) {
         var text = "";
         for(var i in pluginList) {
             if(!pluginList[i].user || userID == pluginList[i].user)
@@ -102,7 +131,7 @@ function injectOptions(oldStyle) {
         }
         doc.width = 570;
         doc.className = "shadow-hover";
-        doc.innerHTML = '<tr><th colspan="3"></th></tr>\
+        doc.innerHTML = '<tr><th colspan="3"><a name="pluglist" id="pluglist"></a></th></tr>\
             <tr><td colspan="3" class="c" style="color: #EEDC82;">Плагины для дополнительных удобств</td></tr>\
             <tr><th colspan="3"></th></tr>\
             <tr><th colspan="3" style="color: #CDB5CD; padding: 0 20px;">В этом разделе собраны плагины от сторонних разработчиков, которые улучшают или дополняют те или иные части интерфейса игры в браузере. Количество плагинов: <span style="color: rgb(255, 165, 0);">' + pluginList.length + '</span></th></tr>\
@@ -133,6 +162,39 @@ function injectOptions(oldStyle) {
     }
 }
 
+/**
+ * Переход к панели настройки плагинов в опциях аккаунта
+ */
+function xgOpenPluginSettings() {
+    if(isNewStyle) {
+        document.location.hash = "#options.php";
+        setTimeout(function() { $("#pluglist")[0].scrollIntoView({
+            behavior: 'smooth'
+        }); }, 1000);
+    }
+    else
+        document.location = "/uni" + universe + "/options.php#pluglist";
+    xgClosePluginInfo();
+}
+
+/**
+ * Закрываем диалог и отмечаем, что он был уже показан
+ */
+function xgClosePluginInfo() {
+    dlgHide('xg-plug-info');
+    window.localStorage.setItem(XG_PLUGIN_AMOUNT_KEY, pluginList.length);
+}
+
+/**
+ * Показываем игроку диалог с полезной информацией или нет
+ */
+function xgNeedShowInfo() {
+    var a = window.localStorage.getItem(XG_PLUGIN_AMOUNT_KEY);
+    return !isSimulate && isBetaTester() && (a == null && !havePlugins && Math.random() < 0.001 || a != null && a != pluginList.length);
+}
+
+var havePlugins = false;
+
 $(document).ready(function () {
     isNewStyle = document.getElementById("stars") != null;
 
@@ -141,13 +203,30 @@ $(document).ready(function () {
         if(window.localStorage.getItem(pname) == "true") {
             console.log("Load plugin:", pluginList[i].title);
             $.getScript("https://xgame.f2h.ru/" + pluginList[i].name + ".js?u=" + userID);
+            havePlugins = true;
         }
     }
 
-    if(modalFormAction) {
+    if(window.modalFormAction) {
         var mfa = modalFormAction;
         modalFormAction = function(r) { mfa(r); injectOptions(false); }
     }
 
     injectOptions(true);
+
+    if(xgNeedShowInfo()) {
+        dlgCreate("xg-plug-info");
+        var text = '<table style="width: 100%;" cellspacing="1"><tr><td class="c" style="height: 20px;"><div style="display: flex;"><div style="flex-grow: 1; cursor: move; user-select: none;" onmousedown="dlgBeginMove(event, \'xg-plug-info\')">Плагины к игре</div><div class="close-btn" title="Закрыть форму" onclick="xgClosePluginInfo()"></div></div></td></tr>\
+          <tr><th></th></tr>';
+        var a = window.localStorage.getItem(XG_PLUGIN_AMOUNT_KEY);
+        if(a != null && a != pluginList.length)
+            text += '<tr><th>В настройках доступны для подключения новые плагины. Вы хотите узнать о них больше?</th></tr>';
+        else
+            text += '<tr><th>Вы знаете о том, что в настройках игры есть раздел с плагинами, подключая которые вы можете менять и улучшать те или иные части интерфейса игры?</th></tr>';
+    
+        text += '<tr><th></th></tr><tr><td class="c" style="padding: 3px;"><button style="cursor: pointer;" onclick="xgOpenPluginSettings()">Настроить плагины</button></td></tr>\
+                </table>';
+        $("#xg-plug-info").html(text);
+        dlgShow("xg-plug-info");
+    }
 });
