85e1bbaddf276369122fdc08a18cef292c139090
Puux <puuxmine@gmail.com>
2019-11-24 01:26:46 +0300

    Вывод списка соалийцев при создании саба

---
+++ alysab.js
@@ -0,0 +1,24 @@
+function loadAllySubList() {
+    if($("input[name=addtogroup]").length) {
+        $.get("/uni" + universe + "/alliance.php?mode=memberslist", function(data){
+            var reg = /OFFSETY, -54\);" onmouseout="return nd\(\);">([^<].*)<\/a/g;
+            var l;
+            var list = document.createElement("datalist");
+            list.id = "mList";
+            $("input[name=addtogroup]").attr("list", "mList");
+            $("input[name=addtogroup]").parent().append(list);
+            while(l = reg.exec(data)) {
+            $(list).append('<option value="' + l[1] + '">');
+            }
+        });
+    }
+}
+
+$(document).ready(function () {
+    loadAllySubList();
+});
+
+if(modalFormAction) {
+    var mfa_sub = modalFormAction;
+    modalFormAction = function(r) { mfa_sub(r); loadAllySubList(); }
+}
