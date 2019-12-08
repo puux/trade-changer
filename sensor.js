c437279163a1daa6ec4852eddf8f2ef4e4b9e78e
Puux <puuxmine@gmail.com>
2019-12-08 11:41:05 +0300

    Последовательный скан планет и стиль строки

--- sensor.js
+++ sensor.js
@@ -51,35 +51,40 @@ window.xgPhTimers = function time_last(){
     
     $(".pha-nodes").remove();
     
+    var scanTimeout = 0;
     var nodes = $("table")[isNewStyle ? 1 : 2].childNodes[0];
     for(var i = 2; i < nodes.childNodes.length-1; i+=2) {
       var planet = nodes.childNodes[i].childNodes[3].innerHTML;
       if(planet && planet.indexOf("Сканировать") >= 0) {
         var planetNum = i/2;
-        var url = "https://xgame-online.com/uni" + universe + "/phalanx.php?galaxy=" + $("input[name=galaxy]").val() + "&system=" + $("input[name=system]").val() + "&planet=" + planetNum;
-        $.get(url, function(row){ return function(data){
-          if(data.indexOf("Флоты не замечены") >= 0) {
-            $('<tr class="pha-nodes"><th colspan=9 class=lolonginactive>Нет летящих флотов</td></tr>').insertAfter(row);
-          }
-          else {
-            var arr = data.match(/<table width=570><th colspan=2><\/th>(.*)<scri/s);
-            if(arr) {
-              var time = data.match(/([\d]+);/g);
-  
-              var text = arr[1];
-              for(var i = 1; i < time.length; i++) {
-                var t = Math.round(new Date().getTime()/1000) + parseInt(time[i-1]);
-                text = text.replace("id=div" + i, "class=pha-time data=" + t);
-              }
-              var rows = '<tr class="pha-nodes"><th colspan="9"></th></tr>' + 
-                text
-                  .replace(/<th colspan=2><\/th><tr><th width=100>/g, '<tr class="pha-nodes"><th colspan=9></th></tr><tr class="pha-nodes"><th colspan="2"></th><th colspan=1>')
-                  .replace(/<tr><th width=100/g, '<tr class="pha-nodes"><th colspan="2"></th><th colspan=1')
-                  .replace(/<th width=430>/g, "<th colspan=6>") + '<tr class="pha-nodes"><th colspan="9"></th></tr>';
-              $(rows).insertAfter(row);
+
+        setTimeout(function(planetNum, row){ return function(){
+          var url = "https://xgame-online.com/uni" + universe + "/phalanx.php?galaxy=" + $("input[name=galaxy]").val() + "&system=" + $("input[name=system]").val() + "&planet=" + planetNum;
+          $.get(url, function(data){
+            if(data.indexOf("Флоты не замечены") >= 0) {
+              $('<tr class="pha-nodes"><th colspan=9 class=lolonginactive>Нет летящих флотов</td></tr>').insertAfter(row);
             }
-          }
-        }}(nodes.childNodes[i]));
+            else {
+              var arr = data.match(/<table width=570><th colspan=2><\/th>(.*)<scri/s);
+              if(arr) {
+                var time = data.match(/([\d]+);/g);
+    
+                var text = arr[1];
+                for(var i = 1; i < time.length; i++) {
+                  var t = Math.round(new Date().getTime()/1000) + parseInt(time[i-1]);
+                  text = text.replace("id=div" + i, "class=pha-time data=" + t);
+                }
+                var rows = '<tr class="pha-nodes"><th colspan="9"></th></tr>' + 
+                  text
+                    .replace(/<th colspan=2><\/th><tr><th width=100>/g, '<tr class="pha-nodes"><th colspan=9></th></tr><tr class="pha-nodes"><th colspan="2"></th><th colspan=1>')
+                    .replace(/<tr><th width=100/g, '<tr class="pha-nodes"><th colspan="2"></th><th colspan=1')
+                    .replace(/<th width=430>/g, "<th colspan=6>") + '<tr class="pha-nodes"><th colspan="9"></th></tr>';
+                $(rows).insertAfter(row);
+              }
+            }
+          });
+        }}(planetNum, nodes.childNodes[i]), scanTimeout);
+        scanTimeout += 500;
       }
     }
     
@@ -91,9 +96,9 @@ window.xgPhTimers = function time_last(){
       var a = window.xgCalcAmountTargets();
       $("#fleetstatusrow").parent().append(
         '<tr>\
-            <th class="c" colspan="1"><span style="color: #CDB5CD">Целей:</span> ' + a + '</th>\
-            <th class="c" colspan="1"><input type="submit" onclick="return xgScanGalaxy()" value="[ Сканировать систему ]"></th>\
-            <th class="c" colspan="1"><span style="color: #CDB5CD">Стоимость:</span> ' + gap(a*5000) + '</th>\
+            <td class="c" colspan="1"><span style="color: #CDB5CD">Целей:</span> ' + a + '</td>\
+            <td class="c" colspan="1"><input type="submit" onclick="return xgScanGalaxy()" value="[ Сканировать систему ]"></td>\
+            <td class="c" colspan="1"><span style="color: #CDB5CD">Затраты:</span> ' + gap(a*5000) + '</td>\
         </tr>');
     }
   }
