645b37de5860f4096154b2c2f214e6fb19289c1e
Puux <puuxmine@gmail.com>
2019-12-09 11:31:15 +0300

    Переработка с учетом всех пожеланий

--- total.js
+++ total.js
@@ -1,159 +1,241 @@
 var panelShow = false;
-var imp = null;
-
 var totalImpRes = { m: 0, c: 0, u: 0 };
+var resTimerId = 0;
+
+function pad(value) {
+    return String(value).split(/(?=(?:...)*$)/).join(" ");
+}
 
 function resStorageKey() {
-  return "xg_res_target_" + universe;
+  return "xg_plug_total_opt_" + universe;
 }
 
-function saveRes() {
-  var res = {
-    m: $("#res-0").val(),
-    c: $("#res-1").val(),
-    u: $("#res-2").val()
+if(window.localStorage.getItem("xg_res_target_" + universe))
+  window.localStorage.removeItem("xg_res_target_" + universe);
+
+var storedOpt = window.localStorage.getItem(resStorageKey());
+var pResOptions = storedOpt ? JSON.parse(storedOpt) : {m: 0, c: 0, u: 0, moon: true, autoLoad: true, showProgress: false, storageLimit: 90};
+
+window.formatRes = function(e) {
+  e.target.value = pad(e.target.value.replace(/ /g, ''));
+}
+
+window.saveRes = function() {
+  pResOptions = {
+    m: $("#res-0").val().replace(/ /g, ''),
+    c: $("#res-1").val().replace(/ /g, ''),
+    u: $("#res-2").val().replace(/ /g, ''),
+    moon: $("#moon-visible")[0].checked,
+    autoLoad: $("#res-auto")[0].checked,
+    showProgress: $("#res-storage")[0].checked,
+    storageLimit: parseInt($("#res-storage-limit").val())
   };
-  if(res.m || res.c || res.u)
-    window.localStorage[resStorageKey()] = JSON.stringify(res);
-  else
-    window.localStorage.removeItem(resStorageKey());
-  updateResTarget();
+  
+  window.localStorage[resStorageKey()] = JSON.stringify(pResOptions);
+  fillResourceDialog();
+  
+  if(pResOptions.autoLoad) {
+    if(!resTimerId)  
+      resTimerId = setInterval(fillResourceDialog, 5000);
+  }
+  else {
+    clearInterval(resTimerId);
+    resTimerId = 0;
+  }
 }
 
-function paramsResDlg() {
+window.paramsResDlg = function () {
   if(!document.getElementById("res-params"))
     dlgCreate("res-params");
   dlgShow("res-params");
 
   var text = '<table style="width: 100%;" cellspacing="1">\
           <tr><td class="c" colspan="2" style="height: 20px;"><div style="display: flex;"><div style="flex-grow: 1; cursor: move; user-select: none;" onmousedown="dlgBeginMove(event, \'res-params\')">Настройки</div><div class="close-btn" title="Закрыть форму" onclick="dlgHide(\'res-params\')"></div></div></td></tr>\
-          <tr><th colspan="2">Установить цель</th></tr>\
-          <tr><th>Металл</th><th><input onblur="saveRes()" id="res-0"></th></tr>\
-          <tr><th>Алмаз</th><th><input onblur="saveRes()" id="res-1"></th></tr>\
-          <tr><th>Уран</th><th><input onblur="saveRes()" id="res-2"></th></tr>';
+          <tr><th colspan="2" style="color: #CDB5CD">Настройки цели</th></tr>\
+          <tr><th>Металл</th><th><input onblur="saveRes()" oninput="formatRes(event)" id="res-0" style="width: 100%;text-align: center;"></th></tr>\
+          <tr><th>Алмаз</th><th><input onblur="saveRes()" oninput="formatRes(event)" id="res-1" style="width: 100%;text-align: center;"></th></tr>\
+          <tr><th>Уран</th><th><input onblur="saveRes()" oninput="formatRes(event)" id="res-2" style="width: 100%;text-align: center;"></th></tr>\
+          <tr><th colspan="2" style="color: #CDB5CD">Прогресс занятости складов</th></tr>\
+          <tr><th>Отображать</th><th><input type="checkbox" oninput="saveRes()" id="res-storage"></th></tr>\
+          <tr><th style="white-space: nowrap;">Предупреждать от</th><th><input onblur="saveRes()" id="res-storage-limit" style="width: 30px;text-align: center;">&nbsp;%</th></tr>\
+          <tr><th colspan="2" style="color: #CDB5CD">Прочие настройки</th></tr>\
+          <tr><th style="white-space: nowrap;">Скрывать луны</th><th><input type="checkbox" oninput="saveRes()" id="moon-visible"></th></tr>\
+          <tr><th>Автообновление</th><th><input type="checkbox" oninput="saveRes()" id="res-auto"></th></tr>';
   
   text += '</table>';     
   $("#res-params").css("width", "220px").html(text);
-  if(window.localStorage[resStorageKey()]) {
-    var res = JSON.parse(window.localStorage[resStorageKey()]);
-    $("#res-0").val(res.m);
-    $("#res-1").val(res.c);
-    $("#res-2").val(res.u);
-  }
+
+  $("#res-0").val(pad(pResOptions.m));
+  $("#res-1").val(pad(pResOptions.c));
+  $("#res-2").val(pad(pResOptions.u));
+  $("#moon-visible")[0].checked = pResOptions.moon;
+  $("#res-auto")[0].checked = pResOptions.autoLoad;
+  $("#res-storage")[0].checked = pResOptions.showProgress;
+  $("#res-storage-limit").val(pResOptions.storageLimit);
 }
 
-function updateResTarget() {
-  if(window.localStorage[resStorageKey()]) {
-    var res = JSON.parse(window.localStorage[resStorageKey()]);
-    for(var type in res) {
-      $("#target-res-" + type).html(pad(res[type]));
-      var total = Math.max(0, res[type] - totalImpRes[type]);
-      $("#target-res-need-" + type).html(pad(total));
+window.updateResTarget = function() {
+  if(pResOptions.m > 0 || pResOptions.c > 0 || pResOptions.u > 0) {
+    for(var type of ["m", "c", "u"]) {
+      if(pResOptions[type] > 0) {
+        $("#target-res-" + type).html(pad(pResOptions[type]));
+        var total = pResOptions[type] - totalImpRes[type];
+        $("#target-res-need-" + type).html((total < 0 ? '-' : '') + pad(Math.abs(total))).css("color", total > 0 ? "#FF6A6A" : "#CDB5CD");
+      }
     }
     $(".res-target").show();
   }
   else {
     $(".res-target").hide();
   }
+  
+  if(pResOptions.moon) {
+    $(".planet-type-3").hide();
+    $(".moons-total").show();
+  } else {
+    $(".planet-type-3").show();
+    $(".moons-total").hide();
+  }
 }
 
-function switchTotalResDlg() {
+function fillResourceDialog() {
+  $.get("https://xgame-online.com/uni" + universe + "/sim.php", function(data){
+    var imp = JSON.parse(data.substring(17));
+    
+    var resImp = {
+      total: {m: 0, c: 0, u: 0},
+      prod: {m: 0, c: 0, u: 0},
+      stor: {m: 0, c: 0, u: 0},
+      moons: {m: 0, c: 0, u: 0}
+    }
+    for(var i in imp.planets) {
+      var planet = imp.planets[i];
+      //planet.storage.metal /= 10;
+      resImp.total.m += planet.resource.metal;
+      resImp.total.c += planet.resource.crystal;
+      resImp.total.u += planet.resource.uran;
+      resImp.prod.m += planet.production.metal;
+      resImp.prod.c += planet.production.crystal;
+      resImp.prod.u += planet.production.uran;
+      resImp.stor.m += planet.storage.metal;
+      resImp.stor.c += planet.storage.crystal;
+      resImp.stor.u += planet.storage.uran;
+
+      if(planet.type == "3") {
+        resImp.moons.m += planet.resource.metal;
+        resImp.moons.c += planet.resource.crystal;
+        resImp.moons.u += planet.resource.uran;
+      }
+    }
+
+    var style = '<style>.res-storage{position: relative;} .res-storage .line{ content: \' \'; position: absolute; background-color: #9ba9c7; height: 1px; left: 0; bottom: -1px; }</style>'
+    var text = style + '<table style="width: 100%;" cellspacing="1">\
+         <tr><td class="c" colspan="5" style="height: 20px;"><div style="display: flex;"><div class="params-btn" title="Настройки" onclick="paramsResDlg()"></div><div style="flex-grow: 1; cursor: move; user-select: none;" onmousedown="dlgBeginMove(event, \'res-panel\')">Сводка по ресурсам</div><div class="close-btn" title="Закрыть форму" onclick="switchTotalResDlg()"></div></div></td></tr>\
+         <tr><th colspan="5"></th>\
+         <tr><th style="width: 16px;"></th><th style="color: #CDB5CD">Название</th><th style="color: #CDB5CD">Металл</th><th style="color: #CDB5CD">Алмаз</th><th style="color: #CDB5CD">Уран</th></tr>';
+
+    for(var i in imp.planets) {
+      var planet = imp.planets[i];
+      
+      var style = 'width: 16px; height: 16px; background-image: url(\'' + planet.image + '\'); background-position-y: ' + (16*planet.y/44) + 'px; background-size: 32px;';
+      if(planet.type == 3)
+        style = 'width: 16px; height: 16px; background-image: url(\'https://xgame-online.com/images/planets/mini_moon.png\'); background-size: 32px;';
+      if(imp.user.img_planet == 1 && !isNewStyle)
+        style = 'width: 16px; height: 16px; background-image: url(\'' + planet.image_old + '\'); background-size: 16px;';
+      
+      var energy = planet.energy < 0 ? ' style="color:#FF6A6A"' : "";
+      var baseURL = isNewStyle ? 'index.php?cp=' + planet.id + '&re=0#buildings.php?gid=' : 'buildings.php?cp=' + planet.id + '&gid=';
+      text += '<tr class="planet-type-' + planet.type + '">\
+              <th><div style="' + style + '"></div></th>\
+              <th><a href="' + baseURL.replace("buildings", "resources") + '" ' + energy + '>' + planet.name + '</a></th>\
+              <th class="res-storage"><a href="' + baseURL + (planet.resource.metal > planet.storage.metal*pResOptions.storageLimit/100 ? 22 : 1) + '">' + makeResourceCell(planet.resource.metal, planet.storage.metal, planet.production.metal) + (planet.type == 1 && pResOptions.showProgress ? makeResourceProgress(planet.resource.metal, planet.storage.metal) : '') + '</a></th>\
+              <th class="res-storage"><a href="' + baseURL + (planet.resource.crystal > planet.storage.crystal*pResOptions.storageLimit/100 ? 23 : 2) + '">' + makeResourceCell(planet.resource.crystal, planet.storage.crystal, planet.production.crystal)+ (planet.type == 1 && pResOptions.showProgress ? makeResourceProgress(planet.resource.crystal, planet.storage.crystal) : '') + '</a></th>\
+              <th class="res-storage"><a href="' + baseURL + (planet.resource.uran > planet.storage.uran*pResOptions.storageLimit/100 ? 24 : 3) + '">' + makeResourceCell(planet.resource.uran, planet.storage.uran, planet.production.uran)+ (planet.type == 1 && pResOptions.showProgress ? makeResourceProgress(planet.resource.uran, planet.storage.uran) : '') + '</a></th>\
+              </tr>';
+    }
+
+    text += '<tr class="moons-total"><th colspan="2">Луны:</th><th>' + pad(resImp.moons.m) + '</th><th>' + pad(resImp.moons.c) + '</th><th>' + pad(resImp.moons.u) + '</th></tr>\
+      <tr><td class="c" colspan="2">Всего:</td>\
+            <td class="c">' + makeResourceCell(resImp.total.m, resImp.stor.m, resImp.prod.m) + '</td>\
+            <td class="c">' + makeResourceCell(resImp.total.c, resImp.stor.c, resImp.prod.c) + '</td>\
+            <td class="c">' + makeResourceCell(resImp.total.u, resImp.stor.u, resImp.prod.u) + '</td></tr>\
+      <tr><th colspan="5"></th></tr>\
+      <tr><td class="c" colspan="2">В полете:</td><td class="c" id="fly-res-m"></td><td class="c" id="fly-res-c"></td><td class="c" id="fly-res-u"></td></tr>';
+    totalImpRes.m = resImp.total.m;
+    totalImpRes.c = resImp.total.c;
+    totalImpRes.u = resImp.total.u;
+
+    text += '<tr class="res-target"><th colspan="5"></th></tr>\
+      <tr class="res-target"><td class="c" colspan="2">Цель:</td><td class="c" id="target-res-m"></td><td class="c" id="target-res-c"></td><td class="c" id="target-res-u"></td></tr>\
+      <tr class="res-target"><td class="c" colspan="2">Осталось:</td><td class="c" id="target-res-need-m"></td><td class="c" id="target-res-need-c"></td><td class="c" id="target-res-need-u"></td></tr>';
+
+    text += '</table>';
+    $("#res-panel").html(text);
+
+    $.get("overview.php", function(data){
+      var arr = data.match(/Всего.*ресурсов.*Металл:(.*),.*Алмаз:(.*),.*Уран:(.*)<\/font><\/td/);
+      if(arr) {
+        $("#fly-res-m").html(arr[1]);
+        $("#fly-res-c").html(arr[2]);
+        $("#fly-res-u").html(arr[3]);
+
+        totalImpRes.m += parseInt(arr[1].replace(/&nbsp;| /g, ""));
+        totalImpRes.c += parseInt(arr[2].replace(/&nbsp;| /g, ""));
+        totalImpRes.u += parseInt(arr[3].replace(/&nbsp;| /g, ""));
+
+        updateResTarget();
+      }
+    });
+
+    updateResTarget();
+  });
+}
+
+function makeResourceProgress(amount, storage) {
+  var persent = amount/storage*100;
+  var color = "#9ba9c7";
+  if(persent >= 100) {
+    color = "#FF6A6A"
+  }
+  else if(persent > pResOptions.storageLimit) {
+    color = "rgb(255, 165, 0)"
+  }
+  return '<span class="line" style="width:' + Math.min(persent, 100) + '%; background-color: ' + color + ';"></span>';
+}
+
+function makeResourceCell(amount, storage, production) {
+  var title = pad(amount);
+  var hint = 'onmouseover="return overlib(\'<table width=200>\
+      <tr><td class=h><font color=#CDB5CD>Вместимость</font></td><td class=h>' + pad(storage) + '</td></tr>\
+      <tr><td class=h><font color=#CDB5CD>Добыча в час</font></td><td class=h>' + pad(production) + '</td></tr>\
+      <tr><td class=h><font color=#CDB5CD>Добыча в сутки</font></td><td class=h>' + pad(production*24) + '</td></tr>\
+      </table>\');" onmouseout="return nd();"'
+  
+  var color = "";
+  if(production) {
+    if(amount >= storage) {
+      color = "#FF6A6A"
+    }
+    else if(amount >= storage*0.9) {
+      color = "rgb(255, 165, 0)"
+    }
+  }
+  
+  return '<span ' + hint + (color ? ' style="color: ' + color + '"' : '') + '>' + title + '</span>';
+}
+
+window.switchTotalResDlg = function() {
   panelShow = !panelShow;
   if(panelShow) {
     $("#res-panel").show();
-
-    if(!imp) {
-      imp = document.createElement("iframe");
-      imp.setAttribute('style', 'display: none;');
-      imp.src = "https://xgame-online.com/uni" + universe + "/imperium.php";
-      imp.onload = function(){
-        
-        var doc = imp.contentWindow.document;
-        var node = doc.getElementById("planet_name");
-        var images = doc.getElementById("planet_image");
-        var metal = doc.getElementById("planet_koord").parentNode.childNodes[11];
-        var cry = doc.getElementById("planet_koord").parentNode.childNodes[12];
-        var uran = doc.getElementById("planet_koord").parentNode.childNodes[13];
-        
-        var text = '<table style="width: 100%;" cellspacing="1">\
-             <tr><td class="c" colspan="5" style="height: 20px;"><div style="display: flex;"><div class="params-btn" title="Настройки" onclick="paramsResDlg()"></div><div style="flex-grow: 1; cursor: move; user-select: none;" onmousedown="dlgBeginMove(event, \'res-panel\')">Сводка по ресурсам</div><div class="close-btn" title="Закрыть форму" onclick="switchTotalResDlg()"></div></div></td></tr>\
-             <tr><th colspan="5"></th>';
-        
-        var totalM = "";
-        var totalC = "";
-        var totalU = "";
-        for(var i = 1; i < node.childNodes.length; i++) {
-          if(node.childNodes[i].innerHTML) {
-            var arr = metal.childNodes[i].innerHTML.match(/(.*)Всего:<\/font>(.*)<br><font color=#CDB5CD>За/);
-            if(arr && arr.length == 3) {
-              totalM = arr[2];
-              arr = cry.childNodes[i].innerHTML.match(/(.*)Всего:<\/font>(.*)<br><font color=#CDB5CD>За/);
-              totalC = arr[2];
-              arr = uran.childNodes[i].innerHTML.match(/(.*)Всего:<\/font>(.*)<br><font color=#CDB5CD>За/);
-              totalU = arr[2];
-            }
-            var overflowM = metal.childNodes[i].innerHTML.indexOf("#FF6A6A") > 0;
-            var overflowC = cry.childNodes[i].innerHTML.indexOf("#FF6A6A") > 0;
-            var overflowU = uran.childNodes[i].innerHTML.indexOf("#FF6A6A") > 0;
-
-            var metalStr = metal.childNodes[i].innerHTML.replace(/<font color="#CDB5CD">\/(.*)</, "<");
-            var cryStr = cry.childNodes[i].innerHTML.replace(/<font color="#CDB5CD">\/(.*)</, "<");
-            var uranStr = uran.childNodes[i].innerHTML.replace(/<font color="#CDB5CD">\/(.*)</, "<");
-            if(metal.childNodes[i].innerHTML.indexOf("<a") == 0) {
-              metalStr = '<a' + (overflowM ? ' style="color:#FF6A6A;"' : '') + metalStr.substring(2);
-              cryStr = '<a' + (overflowC ? ' style="color:#FF6A6A;"' : '') + cryStr.substring(2);
-              uranStr = '<a' + (overflowU ? ' style="color:#FF6A6A;"' : '') + uranStr.substring(2);
-            }
-            else {
-              metalStr = metalStr.replace(/<\/font>0/, "");
-              cryStr = cryStr.replace(/<\/font>0/, "");
-              uranStr = uranStr.replace(/<\/font>0/, "");
-            }
-            text += '<tr>\
-                  <th>' + (i == 1 ? "" : images.childNodes[i].innerHTML.replace(/^[0-9]{1,2}/, "").replace("zeroAbsolute", "").replace(/style="width:(.*)px;"/, 'style="width: 16px; height: 16px; background-size: 32px; display: inline-block;"').replace('class="marg"', 'class="marg" style="width: 20px;"').replace("overview", "buildings")) + '</th>\
-                  <th>' + node.childNodes[i].innerHTML + '</th>\
-                  <th>' + metalStr + '</th>\
-                  <th>' + cryStr + '</th>\
-                  <th>' + uranStr + '</th>\
-                  </tr>';
-          }
-        }
-        text += '<tr><td class="c" colspan="2">Всего:</td><td class="c">' + totalM + '</td><td class="c">' + totalC + '</td><td class="c">' + totalU + '</td></tr>\
-          <tr><th colspan="5"></th></tr>\
-          <tr><td class="c" colspan="2">В полете:</td><td class="c" id="fly-res-m"></td><td class="c" id="fly-res-c"></td><td class="c" id="fly-res-u"></td></tr>';
-        totalImpRes.m = parseInt(totalM.replace(/&nbsp;| /g, ""));
-        totalImpRes.c = parseInt(totalC.replace(/&nbsp;| /g, ""));
-        totalImpRes.u = parseInt(totalU.replace(/&nbsp;| /g, ""));
-
-        text += '<tr class="res-target"><th colspan="5"></th></tr>\
-          <tr class="res-target"><td class="c" colspan="2">Цель:</td><td class="c" id="target-res-m"></td><td class="c" id="target-res-c"></td><td class="c" id="target-res-u"></td></tr>\
-          <tr class="res-target"><td class="c" colspan="2">Осталось:</td><td class="c" id="target-res-need-m"></td><td class="c" id="target-res-need-c"></td><td class="c" id="target-res-need-u"></td></tr>';
-
-        text += '</table>';
-        $("#res-panel").html(text);
-
-        $.get("overview.php", function(data){
-          var arr = data.match(/Всего.*ресурсов.*Металл:(.*),.*Алмаз:(.*),.*Уран:(.*)<\/font><\/td/);
-          if(arr) {
-            $("#fly-res-m").html(arr[1]);
-            $("#fly-res-c").html(arr[2]);
-            $("#fly-res-u").html(arr[3]);
-
-            totalImpRes.m += parseInt(arr[1].replace(/&nbsp;| /g, ""));
-            totalImpRes.c += parseInt(arr[2].replace(/&nbsp;| /g, ""));
-            totalImpRes.u += parseInt(arr[3].replace(/&nbsp;| /g, ""));
-
-            updateResTarget();
-          }
-        });
-
-        updateResTarget();
-      };
-      document.body.appendChild(imp);
-    }
+    fillResourceDialog();
+    if(pResOptions.autoLoad)
+      resTimerId = setInterval(fillResourceDialog, 5000);
   }
-  else
+  else {
     $("#res-panel").hide();
+    clearInterval(resTimerId);
+  }
 }
 
 document.addEventListener("keydown", function(e){
@@ -162,15 +244,7 @@ document.addEventListener("keydown", function(e){
   }
 });
 
-if(window.dlgCreate) {
-  dlgCreate("res-panel");
-}
-else {
-  var totalDlg = document.createElement("div");
-  totalDlg.setAttribute('style', 'width: 400px; position: fixed; left: 30%; top: 15%; z-index: 16; box-shadow: 0 0 5px 0px black; background-color: black; display: none;');
-  totalDlg.id = "res-panel";
-  document.body.appendChild(totalDlg);
-}
+dlgCreate("res-panel");
 
 if(window.enableOptionsButton)
   window.enableOptionsButton("total", paramsResDlg);
