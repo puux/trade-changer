79e952ccb6392ccef2fdb4fda8d6b9446d4c40aa
Puux <puuxmine@gmail.com>
2019-12-23 10:50:18 +0300

    Добавление главы альнса в список

--- alysab.js
+++ alysab.js
@@ -1,14 +1,14 @@
 function loadAllySubList() {
     if($("input[name=addtogroup]").length) {
         $.get("/uni" + universe + "/alliance.php?mode=memberslist", function(data){
-            var reg = /OFFSETY, -54\);" onmouseout="return nd\(\);">([^<].*)<\/a/g;
+            var reg = /OFFSETY, -54\);" onmouseout="return nd\(\);">(.*)<\/a/g;
             var l;
             var list = document.createElement("datalist");
             list.id = "mList";
             $("input[name=addtogroup]").attr("list", "mList");
             $("input[name=addtogroup]").parent().append(list);
             while(l = reg.exec(data)) {
-            $(list).append('<option value="' + l[1] + '">');
+                $(list).append('<option value="' + l[1].replace(/<font color=[\w]+>|<\/font>/g, '') + '">');
             }
         });
     }
